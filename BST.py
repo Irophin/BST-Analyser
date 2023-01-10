60b670a12f17cd8ba1ff3735e86c1ba88a51fd88
Steve PENNEC <steve.pennec@etu.univ-lyon1.fr>
2023-01-11 00:23:34 +0100

    feat: added all types of tree traversal

--- BST.py
+++ BST.py
@@ -9,6 +9,8 @@ class BST(Node):
             right(BST) : the right child
     """
 
+
+
     def __init__(self, value, left=None, right=None):
         """ Constructor of the class
             
@@ -18,8 +20,10 @@ class BST(Node):
                 right(BST) : the right child
         """
         self.value = value
-        self.left = left
+        self.left  = left
         self.right = right
+
+
 
     @staticmethod
     def create_bst(tab):
@@ -38,7 +42,9 @@ class BST(Node):
         tree = BST.plant(None, tab[0])
         for i in range(1, len(tab)):
             tree.plant(tab[i])
+
         return tree
+
 
     def plant(self, value) :
         """ Insert a new value in the tree
@@ -64,6 +70,7 @@ class BST(Node):
             else:
                 self.right.plant(value)
 
+
     def size(self):
         """ Returns the size of the tree
             
@@ -74,6 +81,7 @@ class BST(Node):
             return 0
         return 1 + BST.size(self.left) + BST.size(self.right)
     
+
     def height(self):
         """ Returns the height of the tree
             
@@ -87,6 +95,7 @@ class BST(Node):
             BST.height(self.right),
         )
     
+
     def search(self, value):
         """ Search a value in the tree
 
@@ -108,11 +117,88 @@ class BST(Node):
         else:
             return BST.search(self.left,value)
     
+
+
+
+    def inorder(self):
+        """ Print the values of the tree using the Inorder Traversal
+
+            Returns:
+                None : In any cases, especially when the tree has no value
+        """
+        if self is None:
+            return None
+
+        BST.inorder(self.left)
+        print(self.value, end = " ")
+        BST.inorder(self.right)
+        
+
+    
+    def preorder(self):
+        """ Print the values of the tree using the Preorder Traversal
+
+            Returns:
+                None : In any cases, especially when the tree has no value
+        """
+        if self is None:
+            return None
+        
+        print(self.value, end = " ")
+        BST.preorder(self.left)
+        BST.preorder(self.right)
+
+    
+    def postorder(self):
+        """ Print the values of the tree using the Postorder Traversal
+
+            Returns:
+                None : In any cases, especially when the tree has no value
+        """
+        if self is None:
+            return None
+
+        BST.postorder(self.left)
+        BST.postorder(self.right)
+        print(self.value, end = " ")
+
+    
+    def levelorder(self):
+        """ Print the values of the tree using the Levelorder Traversal/Breadth-first Search
+
+            Returns:
+                None : In any cases, especially when the tree has no value
+        """
+        if self is None:
+            return None
+
+        queue = [self]
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            print(current_node.value, end = " ")
+
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            
+            if current_node.right is not None:
+                queue.append(current_node.right)
+
+
+
+""" Test Results """
 if __name__ == "__main__":
-    tab = [21,8,9,3,15,19,20,7,3,2,1,5,6,4,13,14,12,17,16,18]
+    tab  = [ 21, 8, 9, 3, 15, 19, 20, 7, 3, 2, 1, 5, 6, 4, 13, 14, 12, 17, 16, 18]
     tree = BST.create_bst(tab)
 
+    #Basic methods
     print("Tree: ", tree)
     print("Size: ", BST.size(tree))
     print("Height: ", BST.height(tree))
     print("Search 5: ", BST.search(tree, 5))
+
+    #Traversal methods
+    print("\nInorder Traversal: "), tree.inorder()
+    print("\nPreorder Traversal: "), tree.preorder()
+    print("\nPostorder Traversal: "), tree.postorder()
+    print("\nLevelorder Traversal: "), tree.levelorder()
